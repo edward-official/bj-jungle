db63d73db0ef415a3d70e5fcb6fefc8a44b23d27
edwardlee <openmoresome@gmail.com>
2025-09-29 13:21:48 +0900

    2025-09-29

--- week04/1700.py
+++ week04/1700.py
@@ -1,64 +1,50 @@
 import sys
+
 reader = sys.stdin.readline
 writer = sys.stdout.write
 
 class P1700:
     n_outlet = None
-    n_use = None
-    use = None
-    unplug_counter = None
+    n_sequence = None
+    sequence = None
     current_state = None
+    unplug_counter = None
 
     @staticmethod
     def stdin_processor():
-        P1700.n_outlet, P1700.n_use = map(int, reader().split())
-        P1700.use = list(map(int, reader().split()))
+        P1700.n_outlet, P1700.n_sequence = map(int, reader().split())
+        P1700.sequence = list(map(int, reader().split()))
         return
 
     @staticmethod
-    def reusability_checker(upcoming_opening_range, target):
-        if upcoming_opening_range >= P1700.n_use:
-            return False
-        for index in range(upcoming_opening_range, upcoming_opening_range + P1700.n_outlet):
-            if index >= P1700.n_use:
+    def outlet_manager(index):
+        if len(P1700.current_state) < P1700.n_outlet:
+            P1700.current_state.append(P1700.sequence[index])
+            return
+        current_maximum_index = -1
+        target_device = None
+        for plugged_device in P1700.current_state:
+            try:
+                next_index = P1700.sequence.index(plugged_device, index + 1)
+            except ValueError:
+                target_device = plugged_device
                 break
-            elif P1700.use[index] == target:
-                return True
-        return False
-
-    @staticmethod
-    def necessity_checker(current_opening_range, target):
-        current_closing_range = P1700.n_use if P1700.n_use < current_opening_range + P1700.n_outlet else current_opening_range + P1700.n_outlet
-        if target in set(P1700.use[current_opening_range:current_closing_range]):
-            return True
-        return False
-
-    @staticmethod
-    def unplugger(current_opening_range):
+            if next_index > current_maximum_index:
+                current_maximum_index = next_index
+                target_device = plugged_device
+        P1700.current_state.remove(target_device)
         P1700.unplug_counter += 1
-        for target in P1700.current_state:
-            if P1700.necessity_checker(current_opening_range, target):
-                continue
-            elif not P1700.reusability_checker(current_opening_range + P1700.n_outlet, target):
-                P1700.current_state.remove(target)
-                return
-        for target in P1700.current_state:
-            if not P1700.necessity_checker(current_opening_range, target):
-                P1700.current_state.remove(target)
-                return
+        P1700.current_state.append(P1700.sequence[index])
         return
 
     @staticmethod
     def compute():
-        P1700.current_state = set(P1700.use[0:P1700.n_outlet])
+        P1700.current_state = []
         P1700.unplug_counter = 0
-        for opening_range in range(P1700.n_outlet, P1700.n_use, P1700.n_outlet):
-            for use_index in range(opening_range, opening_range + P1700.n_outlet):
-                if use_index >= P1700.n_use:
-                    break
-                elif P1700.use[use_index] not in P1700.current_state:
-                    P1700.unplugger(opening_range)
-                    P1700.current_state.add(P1700.use[use_index])
+        for index, device in enumerate(P1700.sequence):
+            if device in P1700.current_state:
+                continue
+            P1700.outlet_manager(index)
         return
 
     @staticmethod
